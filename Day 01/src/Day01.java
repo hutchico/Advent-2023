c357cfe41fc20823249027c3e4d0b94d69e52af4
hutchico <22420744+hutchico@users.noreply.github.com>
2023-12-01 01:13:45 -0800

    slight refactor pass

--- Day 01/src/Day01.java
+++ Day 01/src/Day01.java
@@ -8,11 +8,10 @@ public class Day01 {
 		Vector<String> lines = new Vector<String>(1);
 		try {
 			inp = new FileInputStream("input.txt");
-			//System.out.println(inp.available());
 			int c;
 			String line = "";
 			while ((c = inp.read()) != -1) {
-	            if (c == 10) {
+	            if (c == 10) { //newline
 	            	lines.add(line);
 	            	line = "";
 	            }
@@ -20,7 +19,6 @@ public class Day01 {
 	            	line += (char)(c);
 	            }
 	        }
-			
 			inp.close();
 		}
 		catch (IOException ioe) {
@@ -45,64 +43,56 @@ public class Day01 {
 					break;
 				}
 			}
-			//System.out.println(lineNum);
 			sum += lineNum;
 		}
 		System.out.println(sum);
 		
-		String[] numbers = {"zero","one","two","three","four","five","six","seven","eight","nine"};
-		String[] nos = {"0","1","2","3","4","5","6","7","8","9"};
+		//Part 2:
+		String[] numbers = {"one","two","three","four","five","six","seven","eight","nine"};
+		String[] nos = {"1","2","3","4","5","6","7","8","9"};
 		int revisedSum = 0;
 		for(int i = 0; i < lines.size(); i++) {
 			int lineNum = 0;
 			int fpos = 99; //arbitrary number > 10
-			int cpos;
-			int npos;
-			int ref = -1;
+			int cpos, npos;
+			int ref = 0;
 			String line = lines.get(i);
-			for(int j = 1; j < 10; j++) {
+			for(int j = 0; j < 9; j++) {
 				int earliest;
 				cpos = line.indexOf(numbers[j]);
 				npos = line.indexOf(nos[j]);
-				if (npos > -1 && cpos > -1) {
+				if (npos > -1 && cpos > -1) 
 					earliest = npos < cpos ? npos : cpos;
-				}
-				else if (cpos < 0 && npos < 0) { //break cond
+				else if (cpos < 0 && npos < 0)  //break cond
 					continue;
-				}
-				else if (npos < 0){
+				else if (npos < 0)
 					earliest = cpos;
-				}
 				else
 					earliest = npos;
-				
 				if (earliest < fpos) {
 					fpos = earliest;
-					ref = j;
+					ref = j + 1;
 				}
 			}
 			lineNum += ref * 10;
+			
 			fpos = -2; //reset
-			ref = -1;
-			for(int j = 1; j < 10; j++) {
+			ref = 0;
+			for(int j = 0; j < 9; j++) {
 				int latest;
 				cpos = line.lastIndexOf(numbers[j]);
 				npos = line.lastIndexOf(nos[j]);
-				if (npos > -1 && cpos > -1) {
+				if (npos > -1 && cpos > -1) 
 					latest = npos > cpos ? npos : cpos;
-				}
-				else if (cpos < 0 && npos < 0) { //break cond
+				else if (cpos < 0 && npos < 0) //break cond
 					continue;
-				}
-				else if (npos < 0){
+				else if (npos < 0)
 					latest = cpos;
-				}
 				else
 					latest = npos;
-				
 				if (latest > fpos) {
 					fpos = latest;
-					ref = j;
+					ref = j + 1;
 				}
 			}
 			lineNum += ref;
