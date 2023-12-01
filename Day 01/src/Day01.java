4bb72794803f28ca62bd6a88a82fa15e3f8eff92
hutchico <22420744+hutchico@users.noreply.github.com>
2023-11-30 22:38:12 -0800

    part 2 commit

--- Day 01/src/Day01.java
+++ Day 01/src/Day01.java
@@ -45,10 +45,70 @@ public class Day01 {
 					break;
 				}
 			}
-			System.out.println(lineNum);
+			//System.out.println(lineNum);
 			sum += lineNum;
 		}
 		System.out.println(sum);
+		
+		String[] numbers = {"zero","one","two","three","four","five","six","seven","eight","nine"};
+		String[] nos = {"0","1","2","3","4","5","6","7","8","9"};
+		int revisedSum = 0;
+		for(int i = 0; i < lines.size(); i++) {
+			int lineNum = 0;
+			int fpos = 99; //arbitrary number > 10
+			int cpos;
+			int npos;
+			int ref = -1;
+			String line = lines.get(i);
+			for(int j = 1; j < 10; j++) {
+				int earliest;
+				cpos = line.indexOf(numbers[j]);
+				npos = line.indexOf(nos[j]);
+				if (npos > -1 && cpos > -1) {
+					earliest = npos < cpos ? npos : cpos;
+				}
+				else if (cpos < 0 && npos < 0) { //break cond
+					continue;
+				}
+				else if (npos < 0){
+					earliest = cpos;
+				}
+				else
+					earliest = npos;
+				
+				if (earliest < fpos) {
+					fpos = earliest;
+					ref = j;
+				}
+			}
+			lineNum += ref * 10;
+			fpos = -2; //reset
+			ref = -1;
+			for(int j = 1; j < 10; j++) {
+				int latest;
+				cpos = line.lastIndexOf(numbers[j]);
+				npos = line.lastIndexOf(nos[j]);
+				if (npos > -1 && cpos > -1) {
+					latest = npos > cpos ? npos : cpos;
+				}
+				else if (cpos < 0 && npos < 0) { //break cond
+					continue;
+				}
+				else if (npos < 0){
+					latest = cpos;
+				}
+				else
+					latest = npos;
+				
+				if (latest > fpos) {
+					fpos = latest;
+					ref = j;
+				}
+			}
+			lineNum += ref;
+			revisedSum += lineNum;
+		}
+		System.out.println(revisedSum);
 	}
 
 }
