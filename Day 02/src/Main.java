097e36614d920df6c7eb71f126413e2867516640
hutchico <22420744+hutchico@users.noreply.github.com>
2023-12-02 17:08:16 -0800

    removed needless complexity

--- Day 02/src/Main.java
+++ Day 02/src/Main.java
@@ -8,7 +8,7 @@ public class Main {
 		//p2 goal: count the greatest number recorded for each color in each set
 		int IDsum = 0;
 		int powSum = 0;
-		int id = 0; //not gonna bother tracking IDs in specific lines
+		int id = 0;
 		final int Rlim = 12;
 		final int Glim = 13;
 		final int Blim = 14;
@@ -19,42 +19,38 @@ public class Main {
 				String line = "";
 				line = inp.next();
 				id += 1;
-				Boolean out = false;
+				Boolean is_impossible = false;
 				int minred = -1;
 				int mingreen = -1;
 				int minblue = -1;
-				String[] single = line.split(":"); //full line
-				String[] groups = single[1].split(";"); //everything after :
-				for(int i = 0; i < groups.length; i++) {
-					String[] each = groups[i].split(",");
-					for(int j = 0; j < each.length; j++) {
-						each[j] = each[j].substring(1,each[j].length()); //strip leading space
-						String[] pull = each[j].split(" "); //split into number + text
-						int num = Integer.valueOf(pull[0]);
-						switch (pull[1]) {
-						case "red":
-							minred = num > minred ? num : minred;
-							if (num > Rlim) 
-								out = true; //part 1 optimization: break if this evaluates true					
-							break;
-						case "green":
-							mingreen = num > mingreen ? num : mingreen;
-							if (num > Glim) 
-								out = true;							
-							break;
-						case "blue":
-							minblue = num > minblue ? num : minblue;
-							if (num > Blim) 
-								out = true;
-							break;
-						}
+				String[] single = line.split(":"); //ignore format text
+				String[] pulls = single[1].split("[,;]"); //split into [number][color] pairs
+				for(int i = 0; i < pulls.length; i++) {
+					String[] pair = pulls[i].split(" "); //there's a leading space that will be pair[0]
+					int num = Integer.valueOf(pair[1]);  //but we're just gonna ignore that
+					switch (pair[2]) {
+					case "red":
+						minred = num > minred ? num : minred;
+						if (num > Rlim) 
+							is_impossible = true; //part 1 optimization: break if this evaluates true					
+						break;
+					case "green":
+						mingreen = num > mingreen ? num : mingreen;
+						if (num > Glim) 
+							is_impossible = true;							
+						break;
+					case "blue":
+						minblue = num > minblue ? num : minblue;
+						if (num > Blim) 
+							is_impossible = true;
+						break;
 					}
 				}
+				
 				powSum += minred * mingreen * minblue;
 				
-				if(out != true) //not found to be impossible
+				if(!is_impossible) //not found to be impossible
 					IDsum += id;
-				
 			}
 			inp.close();
 			System.out.println(IDsum);
