b7a5703df2dba7076753030533ef350e9acb03ff
hutchico <22420744+hutchico@users.noreply.github.com>
2023-12-07 17:03:19 -0800

    minor tweaks + cleanup, p1 still lost

--- Day 07/src/Hand.java
+++ Day 07/src/Hand.java
@@ -3,11 +3,10 @@ import java.util.List;
 
 public class Hand{
 
-	public Hand(String a, Integer b, int c, int d) {
+	public Hand(String a, Integer b, int c) {
 		this.key = a;
 		this.value = b;
 		this.third = c;
-		this.p2prio = d;
 	}
 	
 	public void set_key(String a) {
@@ -22,10 +21,6 @@ public class Hand{
 		this.third = c;
 	}
 	
-	public void set_p2(int d) {
-		this.p2prio = d;
-	}
-	
 	public String get_key() {
 		return key;
 	}
@@ -38,40 +33,26 @@ public class Hand{
 		return third;
 	}
 	
-	public int get_p2() {
-		return p2prio;
-	}
-	
 	public int compare(Hand y) {
-		if(y.get_third() > this.get_third())
-			return 1;
-		else if (y.get_third() < this.get_third())
-			return -1;
-		else { //the two have equal priority, time to match them up char by char
+		if(y.get_third() == this.get_third()) { //the two have equal priority, time to match them up char by char
 			//Character[] c = {'A','K','Q','J','T','9','8','7','6','5','4','3','2'}; //part 1 precedence list, 0 > 12
 			Character[] c = {'A','K','Q','T','9','8','7','6','5','4','3','2','J'}; //part 2 precedence list, 0 > 12
 			List<Character> deck = Arrays.asList(c);
 			for(int i = 0; i < 5; i++) {
-				int tval, yval;
-				tval = deck.indexOf(this.get_key().charAt(i));
-				yval = deck.indexOf(y.get_key().charAt(i));
-				if(tval > yval)
-					return -1;
-				else if (yval > tval)
-					return 1;
-				else
+				int tval = deck.indexOf(this.get_key().charAt(i));
+				int yval = deck.indexOf(y.get_key().charAt(i));
+				if(tval == yval)
 					continue;
+				else
+					return tval - yval < 0 ? 1 : -1;
 			}
 		}
-		return 69420; //compiler please we are never going to return this
+		else
+			return this.get_third() - y.get_third() < 0 ? 1 : -1;
+		return 69420; //compiler please we are never going to return this stop complaining
 	}
-	
-	
-	
 	
 	private String key;
 	private Integer value;
 	private Integer third;
-	private Integer p2prio;
-	
 }
--- Day 07/src/Main.java
+++ Day 07/src/Main.java
@@ -13,87 +13,85 @@ public class Main {
 	public static void main(String[] args) throws FileNotFoundException {
 		FileInputStream file = new FileInputStream("input.txt");
 		Scanner inp = new Scanner(file).useDelimiter("\n");
-		ArrayList<Hand> Hands = new ArrayList<Hand>();
-		//Character[] c = {'A','K','Q','J','T','9','8','7','6','5','4','3','2'}; //part 1 precedence list, 0 > 12
-		//Character[] c = {'A','K','Q','T','9','8','7','6','5','4','3','2','J'}; //part 2 precedence list, 0 > 12
-		//List<Character> deck = Arrays.asList(c);
-		HashMap<Character, Integer> deck = new HashMap<>();
-		deck.put('A',0);
-		deck.put('K',1);
-		deck.put('Q', 2);
-		deck.put('T', 3);
-		deck.put('9', 4);
-		deck.put('8', 5);
-		deck.put('7', 6);
-		deck.put('6', 7);
-		deck.put('5', 8);
-		deck.put('4', 9);
-		deck.put('3', 10);
-		deck.put('2', 11);
-		deck.put('J', 12);
+		ArrayList<Hand> part1;
+		ArrayList<Hand> part2 = new ArrayList<Hand>();
+		HashMap<Character, Integer> deck2 = new HashMap<>();
+		deck2.put('A', 0);
+		deck2.put('K', 1);
+		deck2.put('Q', 2);
+		deck2.put('T', 3); //part 1: n + 1, J = 3
+		deck2.put('9', 4);
+		deck2.put('8', 5);
+		deck2.put('7', 6);
+		deck2.put('6', 7);
+		deck2.put('5', 8);
+		deck2.put('4', 9);
+		deck2.put('3', 10);
+		deck2.put('2', 11);
+		deck2.put('J', 12); 
 		while(inp.hasNext()) {
 			String[] raw = inp.next().split(" ");
-			Hands.add(new Hand(raw[0],Integer.valueOf(raw[1]),-1,-1));
+			part2.add(new Hand(raw[0],Integer.valueOf(raw[1]),-1));
 		}
+		part1 = new ArrayList<Hand>(part2); //working backward because part 1 code is unrecognizable after p2
+		inp.close();
 		
 		//part 1: iterate through Hands + evaluate wins, assign basic rank to each
-		for(int i = 0; i < Hands.size(); i++) {
-			Hand one = Hands.get(i);
-			//System.out.println(one.get_key());
-			HashMap<Character,Integer> nu = new HashMap<Character,Integer>();
+		for(int i = 0; i < part2.size(); i++) {
+			Hand one = part2.get(i);
+			HashMap<Character,Integer> char_counts = new HashMap<Character,Integer>();
 			for(int n = 0; n < 5; n++) {
 				char card = ((String) one.get_key()).charAt(n);
-				if (nu.get(card) != null) 
-					nu.put(card, nu.get(card) + 1);
+				if (char_counts.get(card) != null) 
+					char_counts.put(card, char_counts.get(card) + 1);
 				else
-					nu.put(card, 1);
+					char_counts.put(card, 1);
 			}
-			//System.out.println("dict size: " + nu.size());
 			int jcount;
-			if(nu.get('J') != null) {
-				jcount = nu.get('J');
-				nu.remove('J');
+			if(char_counts.get('J') != null) {
+				jcount = char_counts.get('J');
+				char_counts.remove('J');
 				ArrayList<Pair<Character,Integer>> counts = new ArrayList<Pair<Character,Integer>>();
 				
-				for(Map.Entry<Character, Integer> ea : nu.entrySet()) {
+				for(Map.Entry<Character, Integer> ea : char_counts.entrySet()) {
 					counts.add(new Pair(ea.getKey(),ea.getValue()));
 				}
 				if(counts.size() == 0) {
-					nu.put('J', 5);
+					char_counts.put('J', 5);
 				}
 				else {
 					Collections.sort(counts,new CustomSort());
 					int val = -1;
-					char fchoice = find_char(one.get_key(),deck); //find the highest card suite in hand
+					char fchoice = find_char(one.get_key(),deck2); //find the highest card suite in hand
 					char cchoice = counts.get(0).get_key();
-					char choice = ' ';
+					char choice;
 					if(cchoice == 'J')
 						cchoice = counts.get(1).get_key();
-					if(deck.get(cchoice) > deck.get(fchoice)) {
+					if(deck2.get(cchoice) > deck2.get(fchoice)) {
 						choice = cchoice;
-						val = nu.get(cchoice);
+						val = char_counts.get(cchoice);
 					}
 					else {
 						choice = fchoice;
-						val = nu.get(fchoice);
+						val = char_counts.get(fchoice);
 					}
-					nu.put(choice, val + jcount);
+					char_counts.put(choice, val + jcount);
 				}
 			}
 			
-			switch(nu.size()) {
+			switch(char_counts.size()) {
 			case (1): //five
 				one.set_third(0);
 				break;
 			case(2): //four or full house
 				ArrayList<Pair<Character,Integer>> pos = new ArrayList<Pair<Character,Integer>>();
-				for(Map.Entry<Character, Integer> ea : nu.entrySet()) {
+				for(Map.Entry<Character, Integer> ea : char_counts.entrySet()) {
 					pos.add(new Pair(ea.getKey(),ea.getValue()));
 				}
 				Collections.sort(pos,new CustomSort());
 				char toFind = pos.get(0).get_key();
 				
-				int count = nu.get(toFind);
+				int count = char_counts.get(toFind);
 				if(count == 4 || count == 1)
 					one.set_third(1); //four
 				else
@@ -102,7 +100,7 @@ public class Main {
 			case(3): //either three kind or two pair
 				Integer[] counts = {0,0,0};
 				int which = 0;
-				for(int ea : nu.values()) { //hacky but saves using another arraylist
+				for(int ea : char_counts.values()) { //hacky but saves using another arraylist
 					counts[which] = ea; 
 					which++;
 				}
@@ -119,18 +117,15 @@ public class Main {
 				one.set_third(6);
 				break;
 			}
-			Hands.set(i, one);
+			part2.set(i, one);
 		}
-		Hands.sort((x,y) -> (x.compare(y)));
+		part2.sort((x,y) -> (x.compare(y)));
 		
 		int sum = 0;
-		for(int i = 0; i < Hands.size(); i++) { //oh it's weakest to strongest
-			sum += Hands.get(i).get_value() * (i + 1);
+		for(int i = 0; i < part2.size(); i++) { //oh it's weakest to strongest
+			sum += part2.get(i).get_value() * (i + 1);
 		}
 		System.out.println(sum);
-		
-		
-		
 	}
 	
 	static class CustomSort implements Comparator<Pair<Character,Integer>>{
